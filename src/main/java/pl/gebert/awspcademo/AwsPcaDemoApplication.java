ee053f64c897cbff018ce0301722b69e0d0548e2
Dominik Gebert <dgebert91@gmail.com>
2024-02-13 12:06:51 +0000

    init

---
+++ src/main/java/pl/gebert/awspcademo/AwsPcaDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.gebert.awspcademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPcaDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AwsPcaDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/gebert/awspcademo/AwsPcaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gebert.awspcademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPcaDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
